86f6bd37cb94bc3c89c856ad9726f3b8f79a226d
ichami630 <brandonichami@gmail.com>
2025-03-14 11:26:10 +0100

    class and attribute manipulation

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ para.innerHTML = "<strong>My journey to JS DOM</strong>"
 //changing styles
 heading.style.color = "red"
 heading.style.fontSize = "12px"
+
+//class and attribute manipulation
+heading.classList.add("highlight");
+heading.classList.remove("highlight");
+heading.classList.toggle("highlight");//add class if absent or remove if present
+
+para.setAttribute("title","hover me")
