4bbc584872eb4f2a59298ff3e922cca6c9d71bef
ichami630 <brandonichami@gmail.com>
2025-03-14 11:22:20 +0100

    changing styles

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ heading.textContent = "DOM Manipulation";
 
 const para = document.querySelector(".para");
 para.innerHTML = "<strong>My journey to JS DOM</strong>"
+
+//changing styles
+heading.style.color = "red"
+heading.style.fontSize = "12px"
