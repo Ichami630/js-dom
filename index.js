d629f5032148540fd00a5dc223e2e39df9f470e0
ichami630 <brandonichami@gmail.com>
2025-03-14 11:36:08 +0100

    creating,appending and removing new elements

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//accessing elements
 const heading = document.getElementById("main-heading");
-heading.textContent = "DOM Manipulation";
-
 const para = document.querySelector(".para");
+const button = document.querySelector("button");
+
+//changing contents
+heading.textContent = "DOM Manipulation";
 para.innerHTML = "<strong>My journey to JS DOM</strong>"
 
 //changing styles
@@ -14,3 +17,17 @@ heading.classList.remove("highlight");
 heading.classList.toggle("highlight");//add class if absent or remove if present
 
 para.setAttribute("title","hover me")
+
+//event listeners (the real power)
+button.addEventListener('click', ()=>{
+    alert("button was clicked")
+})
+
+//creating, appending and removing new elements
+const newDiv = document.createElement("div")
+newDiv.textContent = "I'm new here";
+// document.body.appendChild(newDiv)
+//or insert before or after a specific node
+document.body.insertBefore(newDiv,para);
+//remove the newly created element 
+newDiv.remove();
